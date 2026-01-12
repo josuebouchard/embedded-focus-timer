40e5fd2f3a933e39659917fb0ea22ae52ca82d48
Josué Bouchard <7890663+josuebouchard@users.noreply.github.com>
2026-01-11 20:53:32 -0500

    feat: enhance UI task event handling with structured event types

--- main/ui_task.c
+++ main/ui_task.c
@@ -11,8 +11,8 @@
 #define UI_UPDATE_INTERVAL_MS 1000
 
 void ui_task_initialize(ui_context_t *ui_context,
-                        ui_fsm_snapshot_t *first_snapshot) {
-  ui_context->queue = xQueueCreate(1, sizeof(ui_fsm_snapshot_t));
+                        const ui_fsm_snapshot_t *first_snapshot) {
+  ui_context->queue = xQueueCreate(1, sizeof(ui_task_event_t));
   configASSERT(ui_context->queue);
 
   memcpy(&ui_context->snapshot, first_snapshot, sizeof(ui_fsm_snapshot_t));
@@ -32,8 +32,8 @@ static void print_snapshot(ui_context_t *ctx, uint32_t now_ms) {
 
   if (required < 0) {
     ESP_LOGW(UI_TAG, "There has been an encoding problem");
-  } else if (required >= PRINT_BUFFER_SIZE) {
-    ESP_LOGW(UI_TAG, "Buffer truncated! Needed %d bytes, had %d", required,
+  } else if (required >= (int)PRINT_BUFFER_SIZE) {
+    ESP_LOGW(UI_TAG, "Buffer truncated! Needed %d bytes, had %zu", required,
              PRINT_BUFFER_SIZE);
   }
 
@@ -44,21 +44,42 @@ void ui_task(void *args) {
   ui_context_t *context = (ui_context_t *)args;
 
   ESP_LOGI(UI_TAG, "UI Task initialized");
+  ui_task_event_t event;
+  uint32_t now_ms = pdTICKS_TO_MS(xTaskGetTickCount());
 
   while (true) {
-    uint32_t queue_receive_timeout =
+    TickType_t queue_receive_timeout =
         context->snapshot.state == POMODORO_STATE_RUNNING
             ? pdMS_TO_TICKS(UI_UPDATE_INTERVAL_MS)
             : portMAX_DELAY;
-    (void)xQueueReceive(context->queue, &context->snapshot,
-                        queue_receive_timeout);
 
-    TickType_t now_ms = pdTICKS_TO_MS(xTaskGetTickCount());
+    if (xQueueReceive(context->queue, &event, queue_receive_timeout)) {
+      switch (event.type) {
+      case PRINT_STATUS:
+        break; // Don't do anything special
+      case UPDATE_SNAPSHOT:
+        // Update snapshot
+        memcpy(&context->snapshot, &event.data.snapshot,
+               sizeof(ui_fsm_snapshot_t));
+        break;
+      }
+    }
+
+    now_ms = pdTICKS_TO_MS(xTaskGetTickCount());
     print_snapshot(context, now_ms);
   }
 }
 
 void ui_update_snapshot(const ui_context_t *ctx,
-                        const pomodoro_session_t *pomodoro_session) {
-  xQueueOverwrite(ctx->queue, pomodoro_session);
+                        const ui_fsm_snapshot_t *new_snapshot) {
+  ui_task_event_t event = {
+      .type = UPDATE_SNAPSHOT,
+      .data.snapshot = *new_snapshot,
+  };
+  xQueueOverwrite(ctx->queue, &event);
+}
+
+void ui_request_status(const ui_context_t *ctx) {
+  ui_task_event_t event = {.type = PRINT_STATUS};
+  xQueueOverwrite(ctx->queue, &event);
 }
--- main/ui_task.h
+++ main/ui_task.h
@@ -6,6 +6,18 @@
 
 typedef pomodoro_session_t ui_fsm_snapshot_t;
 
+typedef enum ui_task_event_type {
+  UPDATE_SNAPSHOT,
+  PRINT_STATUS,
+} ui_task_event_type_t;
+
+typedef struct ui_task_event {
+  ui_task_event_type_t type;
+  union {
+    ui_fsm_snapshot_t snapshot;
+  } data;
+} ui_task_event_t;
+
 typedef struct ui_context {
   QueueHandle_t queue;
   ui_fsm_snapshot_t snapshot;
@@ -13,11 +25,13 @@ typedef struct ui_context {
 } ui_context_t;
 
 void ui_task_initialize(ui_context_t *ui_context,
-                        ui_fsm_snapshot_t *first_snapshot);
+                        const ui_fsm_snapshot_t *first_snapshot);
 
 void ui_task(void *args);
 
 void ui_update_snapshot(const ui_context_t *ctx,
                         const ui_fsm_snapshot_t *new_snapshot);
+
+void ui_request_status(const ui_context_t *ctx);
 
 #endif // UI_TASK_H
